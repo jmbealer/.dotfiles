f7e46acd965ce75ddca806dbc307f158ad31767a
Jb <77351757+jmbealer@users.noreply.github.com>
2022-04-03 16:44:08 -0500

    some cleaning and literate coding

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -116,7 +116,7 @@
        ;;beancount         ; mind the GAAP
        (cc +lsp)                ; C > C++ == 1
        (clojure +lsp)           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
@@ -188,5 +188,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
