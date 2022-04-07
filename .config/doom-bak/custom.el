10ba1bc4677d9a50253eab87bb26f0d74ddd77d1
Jb <77351757+jmbealer@users.noreply.github.com>
2022-04-06 21:09:02 -0500

    updated doom and mess with channels

---
+++ .config/doom-bak/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  '(org-roam-capture-templates
+     '(("d" "default" plain "%?" :target
+         (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}
+#+date: %U
+")
+         :unnarrowed t)))
+ '(org-startup-folded 'content)
+  '(package-selected-packages
+     '(sqlite org-roam org-roam-ui emacsql-mysql emacsql git-commit irony esqlite emacsql-libsqlite3 sqlite3 pg finalize emacsql-sqlite3))
+ '(tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
