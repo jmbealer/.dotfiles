0d42e94871c82e04d717930bc499ccc84adeaf2b
Jb <77351757+jmbealer@users.noreply.github.com>
2022-04-08 14:55:03 -0500

    doom update

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-clojure-custom-server-command '("bash" "-c" "~/.bin/clojure-lsp"))
+ '(lsp-clojure-custom-server-command '("bash" "-c" "~/.bin/clojure-lsp") t)
  '(lsp-go-gopls-server-path "~/go/bin/gopls")
  '(org-roam-capture-templates
     '(("d" "default" plain "%?" :target
@@ -13,7 +13,7 @@
         :unnarrowed t)))
  '(org-startup-folded 'content)
  '(package-selected-packages
-    '(go-complete go go-guru sqlite org-roam org-roam-ui emacsql-mysql emacsql git-commit irony esqlite emacsql-libsqlite3 sqlite3 pg finalize emacsql-sqlite3))
+    '(company-go company-native-complete auto-complete-clang native-complete bash-completion go-complete go go-guru sqlite org-roam org-roam-ui emacsql-mysql emacsql git-commit irony esqlite emacsql-libsqlite3 sqlite3 pg finalize emacsql-sqlite3))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
