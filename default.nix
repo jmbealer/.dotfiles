388eba64c9488ad1093a2d473db4dabcc86e850b
Justin Bealer <jmbealer11@gmail.com>
2022-07-29 23:28:23 -0500

    move dotfiles to etc

--- default.nix
+++ default.nix
@@ -57,8 +57,9 @@ with lib.my;
 
   # Use the latest kernel
   boot = {
+    kernelPackages = mkDefault pkgs.linuxPackages_hardened;
     # kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_5_16;
-    kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_5_18;
+    # kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_5_18;
     loader = {
       efi.canTouchEfiVariables = mkDefault true;
       systemd-boot.configurationLimit = 10;
