22c376af2ec55dbf06c8ca01c0437854e5fcef27
Justin Bealer <jmbealer11@gmail.com>
2022-08-03 20:32:02 -0500

    working on emacs

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -1,91 +1,48 @@
-{ config, pkgs, location, ... }:
-
+{ config, pkgs, callPackage, ... }:
 { 
-  services.emacs.enable = true;
 
-  system.userActivationScripts = {
-    doomEmacs = {
-      text = ''
-        source ${config.system.build.setEnvironment}
-        DOOM="$HOME/.emacs.d"
+  services.emacs.package = pkgs.emacsUnstable;
 
-        if [ ! -d "$DOOM" ]; then
-          git clone https://github.com/hlissner/doom-emacs.git $DOOM
-          yes | $DOOM/bin/doom install
-          rm -r $HOME/.doom.d
-          ln -s ${location}/modules/editors/emacs/doom.d $HOME/.doom.d
-          $DOOM/bin/doom sync
-        else
-          $DOOM/bin/doom sync
-        fi
-      '';
-    };
-  };
-
-  # nixpkgs.overlays = [
-    # (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
-  # ];
-
-  environment.systemPackages = with pkgs; [
-    # emacs
-    ripgrep
-    # coreutils
-    coreutils-full
-    fd
-
-    # emacsGcc
-    # emacsPgtkGcc
-    emacsPgtkNativeComp 
-    # emacsNativeComp
-    # emacsNativeComp
-    sqlite
-    pandoc
-    emacs28Packages.emacsql
-    emacs28Packages.emacsql-sqlite
-    emacs28Packages.emacsql-sqlite-module
-    emacs28Packages.emacsql-sqlite-builtin
-    lispPackages.clsql-sqlite3
-
-
-      ## Emacs itself
-      # binutils       # native-comp needs 'as', provided by this
-      # 29 + pgtk + native-comp
-      # ((emacsPackagesFor emacsPgtkGcc).emacsWithPackages (epkgs: [
-
-      ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (epkgs: [
-        epkgs.vterm
-      ]))
-
-      ## Doom dependencies
-      git
-      # (ripgrep.override {withPCRE2 = true;})
-      gnutls              # for TLS connectivity
-
-      ## Optional dependencies
-      # fd                  # faster projectile indexing
-      imagemagick         # for image-dired
-      # (mkIf (config.programs.gnupg.agent.enable)
-        # pinentry_emacs)   # in-emacs gnupg prompts
-      zstd                # for undo-fu-session/undo-tree compression
-
-      ## Module dependencies
-      # :checkers spell
-      (aspellWithDicts (ds: with ds; [
-        en en-computers en-science
-      ]))
-      # :tools editorconfig
-      editorconfig-core-c # per-project style config
-      # :tools lookup & :lang org +roam
-      # sqlite
-      # :lang latex & :lang org (latex previews)
-      texlive.combined.scheme-medium
-      # :lang beancount
-      # beancount
-      # unstable.fava  # HACK Momentarily broken on nixos-unstable
-
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      sha256 = "09wv9mfb94ac19mbb6s3b3kkq52jk4475l4lh4gdxb3kk3j2bwsd";
+    }))
   ];
 
+  # services.emacs.package = with pkgs; ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (epkgs: [ epkgs.vterm ]));
+
+  services.emacs.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    ripgrep
+    coreutils
+    fd
+    (emacsWithPackagesFromUsePackage { 
+      config = ./doom.d/config.el;
+      package = pkgs.emacsGitNativeComp;
+      alwaysEnsure = true;
+      alwaysTangle = true;
+      extraEmacsPackages = epkgs: [
+        epkgs.cask
+      ];
+    })
+  ];
+  # environment.systemPackages = [
+
+    # (emacsWithPackagesFromUsePackage {
+      # config = # rm -r $HOME/.doom.d
+      # package = pkgs.emacsGit;
+
+      # package = pkgs.emacsGitNativeComp;
+      # alwaysEnsure = true;
+      # alwaysTangle = true;
+      # extraEmacsPackages = epkgs: [
+        # epkgs.cask
+      # ];
+    # })
+  # ];
+
     fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
-    services.emacs.package = with pkgs; ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (epkgs: [ epkgs.vterm ]));
 
 }
